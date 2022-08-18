5702aa4acece7714fd502cbb8f8b136fab92ffbc
Huong Bui Vu <hbui-vu@lab2r2s7.42abudhabi.ae>
2022-08-18 19:02:03 +0400

    updated C08

--- C08/ex01/ft_boolean.h
+++ C08/ex01/ft_boolean.h
@@ -6,7 +6,7 @@
 /*   By: hbui-vu <huong.buivu@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 14:18:33 by hbui-vu           #+#    #+#             */
-/*   Updated: 2022/08/18 15:26:21 by hbui-vu          ###   ########.fr       */
+/*   Updated: 2022/08/18 18:57:50 by hbui-vu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 #define __FT_BOOLEAN_H__
 
 #define t_bool int
-#define EVEN(nbr) (nbr / 2 == 0)
+#define EVEN(nbr) (nbr % 2)
+#define TRUE 0
+#define FALSE 1
 #define EVEN_MSG "I have an even number of arguments"
 #define ODD_MSG "I have an odd number of arguments"
 #define SUCCESS 0
